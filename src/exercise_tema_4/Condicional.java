4a69bac72893f81885d5e2a9b74b638c1a1d495c
Julián España <julii1295@gmail.com>
2022-09-19 21:43:35 -0500

    Terminando primer punto del segundo taller

--- src/exercise_tema_4/Condicional.java
+++ src/exercise_tema_4/Condicional.java
@@ -1,5 +1,9 @@
 package exercise_tema_4;
 
+/*Usando un if, crear una condición que compare si 
+la variable numeroIf es positivo, negativo, o 0
+*/
+
 import java.util.Scanner;
 
 public class Condicional {	
@@ -7,14 +11,21 @@ public class Condicional {
 	
 	public static void numeroIf () {
 		int numeroIf = 0;
+		int neutro = 0;
 		
 		System.out.print("Digite el número a evaluar: ");
 		numeroIf = entrada.nextInt();
 		
-		if (numeroIf >= 0) {
+		if (numeroIf > 100) {
+			System.out.println("No se permiten número mayores a 100. Vuelva a ejecutar el programa, por favor");
+			System.exit(0);
+		}
+		if (numeroIf > 0) {
 			System.out.println("Número positivo");
 		} else if (numeroIf < 0){
 			System.out.println("Número negativo");
+		} else if (numeroIf == neutro) {
+			System.out.println("El número ingresado es cero");
 		}
 	}
 }
--- src/exercise_tema_4/Main.java
+++ src/exercise_tema_4/Main.java
@@ -12,25 +12,21 @@ public class Main {
 		numero.numeroIf();
 		
 		int opc = 0;
+		
 		do {
 			System.out.println("¡Quiere volver a ejecutar? 1=si / 2=no");
 			opc = entrada.nextInt();
 			
 			while (opc == 1) {
+				numero.numeroIf();
+				
 				System.out.println("¡Quiere volver a ejecutar? 1=si / 2=no");
 				opc = entrada.nextInt();
-				
-				if (opc == 1) {
-					numero.numeroIf();				
-				}
 			}			
-			
 			if (opc == 2) {
 				System.out.println("Hasta luego");
 				System.exit(0);
-			}				
-			
-		} while (numero.equals(-1));
-		
+			}			
+		} while (opc != -10000);
 	}	
 }
